91775fbb90749a9555ed9209eaa9d9237e4bd2ff
Muhammad Shahraim Khan Niazi <120953592+shahraim@users.noreply.github.com>
2024-01-25 21:06:12 +0500

    delete some line

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ function App() {
     <>
     <div className="text-6xl font-bold">Hello Malik World! (dropdown) New Change</div>
     <p className="text-2xl text-rose-600">This is new changes</p>
-    <p className="text-2xl text-rose-600">This changes is on feature-C (cluster)</p>
-    <p className="text-2xl text-rose-600">This cis new hanges is on feature-C (cluster)</p>
     <p className="text-2xl text-rose-600">This shah branch</p>
     </>
 
