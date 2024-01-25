e62be01fc676fb05aa03ba11bf53d1ec2e0cf2df
Muhammad Shahraim Khan Niazi <120953592+shahraim@users.noreply.github.com>
2024-01-25 19:57:02 +0500

    add dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     <div className="text-6xl font-bold">Hello Malik World! (dropdown) New Change</div>
-    <p className="text-2xl text-rose-600">This is new changes</p>
+    <p className="text-2xl text-rose-600">This is new changes (dropdown)</p>
     </>
   )
 }
